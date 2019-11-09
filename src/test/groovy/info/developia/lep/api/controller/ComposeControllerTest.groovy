eac363bf2f66444c9e46426c50a676f9c737306d
manudevelopia <manu@developia.info>
2019-11-09 14:12:35 +0100

    test: update test

--- src/test/groovy/info/developia/lep/api/controller/ComposeControllerTest.groovy
+++ src/test/groovy/info/developia/lep/api/controller/ComposeControllerTest.groovy
@@ -1,13 +1,22 @@
 package info.developia.lep.api.controller
 
-import spock.lang.*
+import info.developia.lep.api.service.ComposeService
+import spock.lang.Specification
 
 class ComposeControllerTest extends Specification {
+    ComposeController composeController
+    ComposeService composeService
+
+    def setup() {
+        composeService = Mock()
+        composeController = new ComposeController(composeService)
+    }
 
     def "test get All"() {
+        given:
+        composeService.getAll() >> "<h2>Hello world!!</h2>"
         when:
-        def result = ComposeController.getAll("req", "res")
-
+        def result = composeController.getAll("req", "res")
         then:
         result == "<h2>Hello world!!</h2>"
     }
